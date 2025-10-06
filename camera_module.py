9e33ba04cdb887677eabdae59c6c8cc87290e5e2
Kyle <kylejlewis55@gmail.com>
2025-10-06 16:34:32 -0700

    Test Camera

--- camera_module.py
+++ camera_module.py
@@ -8,7 +8,7 @@ import cv2
 import time
 
 class PiCameraCapture:
-    def __init__(self, resolution=(4608, 2592), warmup_time=2):
+    def __init__(self, resolution=(1920, 1080), warmup_time=2):
         self.picam2 = Picamera2()
         self.picam2.preview_configuration.main.size = resolution
         self.picam2.preview_configuration.main.format = "RGB888"
