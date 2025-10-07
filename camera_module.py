4636c20350919ec9ae1e6910200513c43ab240a6
Kyle <kylejlewis55@gmail.com>
2025-10-07 09:30:30 -0700

    Camera Scale

--- camera_module.py
+++ camera_module.py
@@ -8,21 +8,42 @@ import cv2
 import time
 
 class PiCameraCapture:
-    def __init__(self, resolution=(4608, 2592), warmup_time=2):
+    def __init__(self, mode="scan", warmup_time=2):
         self.picam2 = Picamera2()
-        self.picam2.configure(self.picam2.create_still_configuration(
-            main={"size": resolution, "format": "RGB888"}
-        ))
         self.warmup_time = warmup_time
+
+        if mode == "preview":
+            self.picam2.configure(self.picam2.create_preview_configuration(
+                main={"size": (1280, 720), "format": "RGB888"}
+            ))
+        elif mode == "scan":
+            self.picam2.configure(self.picam2.create_still_configuration(
+                main={"size": (4608, 2592), "format": "RGB888"},
+                display="off"
+            ))
+
+        # 🔧 Set image quality controls here
+        self.picam2.set_controls({
+            "Sharpness": 1.0,
+            "Contrast": 1.0,
+            "Saturation": 1.0,
+            "NoiseReductionMode": 2,  # High quality
+            "AwbMode": 1              # Auto white balance
+        })
 
     def start(self):
         self.picam2.start()
         time.sleep(self.warmup_time)
 
     def capture_frame(self):
-        frame = self.picam2.capture_array()
-        resized = cv2.resize(frame, (960, 540), interpolation=cv2.INTER_AREA)
-        return resized
+        return self.picam2.capture_array()
 
     def stop(self):
         self.picam2.stop()
+
+'''
+    def capture_frame(self):
+        frame = self.picam2.capture_array()
+        resized = cv2.resize(frame, (960, 540), interpolation=cv2.INTER_AREA)
+        return resized
+'''
