cb34752783d48a99311565464dc9716f598de408
Kyle <kylejlewis55@gmail.com>
2025-10-08 11:11:32 -0700

    Create Live Preview

--- scanner/cli.py
+++ scanner/cli.py
@@ -4,6 +4,8 @@ import cv2
 from .capture import capture_with_rpicam_still
 from .pipeline import compute_pipeline_steps
 from .preview import show_pipeline_grid_with_ocr
+from scanner.live_preview import run_live_preview
+
 
 def main():
     p = argparse.ArgumentParser()
@@ -11,13 +13,15 @@ def main():
     p.add_argument("--file", type=str, help="Path to image file to preview instead of capturing")
     args = p.parse_args()
 
-    if args.file:
+    run_live_preview()
+
+    '''if args.file:
         img = cv2.imread(args.file)
     else:
         img = capture_with_rpicam_still("capture_for_preview.jpg")
 
     steps = compute_pipeline_steps(img)
-    show_pipeline_grid_with_ocr(img)
+    show_pipeline_grid_with_ocr(img)'''
 
 if __name__ == "__main__":
     main()
