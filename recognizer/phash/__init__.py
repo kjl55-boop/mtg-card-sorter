bca248f3d6f18ee6114544d4799b683c830278c5
Kyle <kylejlewis55@gmail.com>
2025-10-17 13:33:33 -0700

    Remove OCR and ORB from main pipeline / Reorg capture / reorg phash

--- recognizer/phash/__init__.py
+++ recognizer/phash/__init__.py
@@ -1,4 +1,4 @@
 # recognizer/phash/__init__.py
-from .phash import match_card
+from .phash import Matcher
 from .phash_tools import PhashComparator
 from .phash_bridge import normalize_and_save, compute_phash_bgr
